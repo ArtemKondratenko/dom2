b9f291ba4a428d7d2710f7fd31f014383c34d2bf
Артем Кондратенко <artem-osteo@yandex.ru>
2022-04-06 19:05:32 +0300

    Todo 2.1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int tickets  = 100;
+        int rubMil = 20;
+
+        int numberMiles = (tickets / rubMil);
+
+
+
+        System.out.println(numberMiles);
+
+
+    }
+}
